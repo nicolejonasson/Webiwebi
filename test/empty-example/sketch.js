005e21a98d4443e461592f2a42558158ef997d48
nicolejonasson <38101829+nicolejonasson@users.noreply.github.com>
2018-04-15 18:56:57 +0200

    Update sketch.js

--- test/empty-example/sketch.js
+++ test/empty-example/sketch.js
@@ -1,22 +1,14 @@
-let angle = 0;
+
 
 function setup() {
-  createCanvas(500,400,WEBGL);
-  background(150);
-  // put setup code here
+  createCanvas(500,400);
+
 }
 
 function draw() {
   background(150);
-  ambientLight(100);
-  directionalLight(255,255,255,1,-1,0);
-  rotateX(angle);
-  rotateY(angle*1.3);
-  rotateZ(angle*0.7);
-  //rect(0,0,150,150);
-  box(100);
+  fill(255,0,0);
+  rect(20,20,100,100);
 
-
-  angle += 0.07;
 
 }
